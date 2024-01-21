09736ba32827678087efdccb988b5a42816c336d
matheus franco <matheusfortunatoaw@gmail.com>
2024-01-21 14:17:25 -0300

    nodemailer change

--- server.js
+++ server.js
@@ -1,67 +1,59 @@
 const express = require("express");
-require("dotenv").config();
 const bodyParser = require("body-parser");
-const sgMail = require("@sendgrid/mail");
+const nodemailer = require("nodemailer");
+const smtpTransport = require("nodemailer-smtp-transport");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-
-// Use o body-parser para processar dados do formulário
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Configure a API Key do SendGrid
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const transporter = nodemailer.createTransport(smtpTransport({
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  secure: process.env.EMAIL_SECURE === "true",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+}));
 
-// Rota para lidar com o envio de mensagens
-app.post("/enviar-mensagem", (req, res) => {
+app.post("/enviar-mensagem", async (req, res) => {
   const { nome, email, numero, mensagem } = req.body;
 
-  // Certifique-se de que os dados são recebidos corretamente
   if (!nome || !email || !numero || !mensagem) {
-    return res
-      .status(400)
-      .json({
-        success: false,
-        message: "Por favor, forneça todos os campos necessários.",
-      });
+    return res.status(400).json({
+      success: false,
+      message: "Por favor, forneça todos os campos necessários.",
+    });
   }
 
-  // Configuração do e-mail
-  const msg = {
-    to: "matheusfortunatoaw@outlook.com", // Coloque o seu e-mail aqui
-    from: "matheusfortunatoaw@hotmail.com",
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: process.env.EMAIL_USER, // Substitua pelo seu endereço de e-mail para receber os dados
     subject: "Nova mensagem de contato",
     text: `Nome: ${nome}\nEmail: ${email}\nNúmero: ${numero}\nMensagem: ${mensagem}`,
   };
 
-  // Envio do e-mail
-  sgMail
-    .send(msg)
-    .then(() => {
-      console.log("E-mail enviado com sucesso");
-      res.json({ success: true, message: "Mensagem enviada com sucesso!" });
-    })
-    .catch((error) => {
-      console.error(
-        "Erro ao enviar e-mail:",
-        error.response ? error.response.body : error.message
-      );
-      res
-        .status(500)
-        .json({ success: false, message: "Erro ao enviar a mensagem" });
-    });
+  try {
+    // Envio do e-mail
+    const info = await transporter.sendMail(mailOptions);
+    console.log("E-mail enviado com sucesso:", info);
+    res.json({ success: true, message: "Mensagem enviada com sucesso!" });
+  } catch (error) {
+    console.error("Erro ao enviar e-mail:", error.message);
+    res.status(500).json({ success: false, message: "Erro ao enviar a mensagem" });
+  }
 });
 
-// Rota para a página inicial
 app.get("/", (req, res) => {
   res.send("Bem-vindo à página inicial do seu aplicativo!");
 });
 
-// Inicie o servidor
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
